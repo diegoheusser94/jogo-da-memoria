f57bde286b94c0e5ac5e194f629f185045269a4b
Diego <diego.heusser@gmail.com>
2016-09-20 10:34:56 -0300

    Eventos dos botões Iniciar e fechar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-﻿var personagens = ["coyote.jpg",
+﻿$(document).ready(function () {
+	
+	//Variaveis
+	var personagens = ["coyote.jpg",
 				   "dk.jpg",
 				   "jerry.png",
 				   "luigi.jpg",
@@ -19,7 +22,11 @@
 				   "tom.png",
 				   "woody.jpg"];
 
-function embaralhar() {
+	var player1 {name:"Jogador 1", points:0};
+	var player2 {name:"Jogador 2", points:0};
+
+	//Funções
+	function embaralhar() {
     var j, x, i;
     for (i = personagens.length; i; i--) {
         j = Math.floor(Math.random() * i);
@@ -27,20 +34,45 @@ function embaralhar() {
         personagens[i - 1] = personagens[j];
         personagens[j] = x;
     }
-}
-
-function setImagens(){
-	var i, b;
-	for (i = 0; i < personagens.length; i++) {
-		document.getElementById("img-"+(i+1)).src = "img/"+personagens[i];
 	}
-}
 
-function revelar(id) {
-	var i = Number(id.substring(4,id.length));
-	document.getElementById(id).src = "img/"+personagens[i-1];
-}
+	function setImagens(){
+		var i, b;
+		for (i = 0; i < personagens.length; i++) {
+			document.getElementById("img-"+(i+1)).src = "img/"+personagens[i];
+		}
+	}
 
-function desvirarCarta(id){
+	function revelar(id) {
+		var i = Number(id.substring(4,id.length));
+		document.getElementById(id).src = "img/"+personagens[i-1];
+	}
+
+	function desvirarCarta(id){
 	document.getElementById(id).src = "img/verso.jpg";
-}
+	}
+
+	//Eventos
+	$('#btn-start').click(function (){
+		var name_user_1 = $('#edit-name-user-1').val();
+		var name_user_2 = $('#edit-name-user-2').val();
+
+		if(name_user_1 != '' && name_user_2 != ''){
+			player1 = {name:name_user_1, points:0};
+			player2 = {name:name_user_2, points:0};
+		} else {
+			$('#alert-warning').show();
+		}
+	});
+
+	$('#btn-close').click(function (){
+		player1 {name:"Jogador 1", points:0};
+		player2 {name:"Jogador 2", points:0};
+		$('#edit-name-user-1').val("");
+		$('#edit-name-user-2').val("");
+		$('#alert-warning').hide();
+	});
+});
+
+
+
